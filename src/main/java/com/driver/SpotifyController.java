234180c27e67f806a95d9323e84442b5158133ad
spandit230496 <sandip.pandit230496@gmail.com>
2023-03-05 14:49:10 +0530

    error

--- src/main/java/com/driver/SpotifyController.java
+++ src/main/java/com/driver/SpotifyController.java
@@ -21,7 +21,7 @@ public class SpotifyController {
     @PostMapping("/add-artist")
     public String createArtist(@RequestParam(name = "name") String name){
         //create the artist with given name
-
+        spotifyService.createArtist(name);
         return "Success";
     }
 
@@ -29,7 +29,7 @@ public class SpotifyController {
     public String createAlbum(@RequestParam(name = "title") String title, String artistName){
         //If the artist does not exist, first create an artist with given name
         //Create an album with given title and artist
-
+        spotifyService.createAlbum(title, artistName);
         return "Success";
     }
 
@@ -37,7 +37,7 @@ public class SpotifyController {
     public String createSong(String title, String albumName, int length) throws Exception{
         //If the album does not exist in database, throw "Album does not exist" exception
         //Create and add the song to respective album
-
+        spotifyService.createSong(title, albumName, length);
         return "Success";
     }
 
@@ -46,7 +46,7 @@ public class SpotifyController {
         //Create a playlist with given title and add all songs having the given length in the database to that playlist
         //The creater of the playlist will be the given user and will also be the only listener at the time of playlist creation
         //If the user does not exist, throw "User does not exist" exception
-
+        spotifyService.createPlaylistOnLength(mobile, title, length);
         return "Success";
     }
 
@@ -55,7 +55,7 @@ public class SpotifyController {
         //Create a playlist with given title and add all songs having the given titles in the database to that playlist
         //The creater of the playlist will be the given user and will also be the only listener at the time of playlist creation
         //If the user does not exist, throw "User does not exist" exception
-
+        spotifyService.createPlaylistOnName(mobile, title, songTitles);
         return "Success";
     }
 
@@ -66,31 +66,32 @@ public class SpotifyController {
         //If the user does not exist, throw "User does not exist" exception
         //If the playlist does not exists, throw "Playlist does not exist" exception
         // Return the playlist after updating
-
+        spotifyService.findPlaylist(mobile, playlistTitle);
         return "Success";
     }
 
-    @PutMapping("/like-song")
-    public String likeSong(String mobile, String songTitle) throws Exception{
-        //The user likes the given song. The corresponding artist of the song gets auto-liked
-        //A song can be liked by a user only once. If a user tried to like a song multiple times, do nothing
-        //However, an artist can indirectly have multiple likes from a user, if the user has liked multiple songs of that artist.
-        //If the user does not exist, throw "User does not exist" exception
-        //If the song does not exist, throw "Song does not exist" exception
-        //Return the song after updating
+//    @PutMapping("/like-song")
+//    public String likeSong(String mobile, String songTitle) throws Exception{
+//        //The user likes the given song. The corresponding artist of the song gets auto-liked
+//        //A song can be liked by a user only once. If a user tried to like a song multiple times, do nothing
+//        //However, an artist can indirectly have multiple likes from a user, if the user has liked multiple songs of that artist.
+//        //If the user does not exist, throw "User does not exist" exception
+//        //If the song does not exist, throw "Song does not exist" exception
+//        //Return the song after updating
+//        spotifyService.likeSong(mobile, songTitle);
+//        return "Success";
+//    }
 
-        return "Success";
-    }
-
-    @GetMapping("/popular-artist")
-    public String mostPopularArtist(){
-        //Return the artist name with maximum likes
-      return "";
-    }
-
-    @GetMapping("/popular-song")
-    public String mostPopularSong(){
-        //return the song title with maximum likes
-  return "";
-    }
+//    @GetMapping("/popular-artist")
+//    public String mostPopularArtist(){
+//        //Return the artist name with maximum likes
+//        return spotifyService.mostPopularArtist();
+//    }
+//
+//    @GetMapping("/popular-song")
+//    public String mostPopularSong(){
+//        //return the song title with maximum likes
+//        return spotifyService.mostPopularSong();
+//
+//    }
 }
